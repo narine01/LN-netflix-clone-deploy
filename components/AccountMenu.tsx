86db22cd95bfde80f4e5cd0fcbe611f3be8fe389
Laxmy Narayan <narayanlakshmi721@gmail.com>
2024-01-15 17:57:39 +0530

    changes for vercel deployment build

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -7,7 +7,7 @@ interface AccountMenuProps {
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
-    const {data} = useCurrentUser();
+    const { data: currentUser, mutate } = useCurrentUser();
     if (!visible) return null;
     return (
         <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
@@ -15,7 +15,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
                 <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
                     <img className="w-8 rounded-md" src="/images/default-blue.png" alt="profile" />
                     <p className="text-white text-sm group-hover/item:underline">
-                        {data?.name}
+                        {currentUser?.name}
                     </p>
                 </div>
                 <hr className="bg-gray-600 border-0 h-px my-4" />
